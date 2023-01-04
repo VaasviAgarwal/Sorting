72250ea56e309be779897b491072c25fad4a1e5d
Vaasvi Agarwal <83585127+VaasviAgarwal@users.noreply.github.com>
2023-01-04 14:10:56 +0530

    Update bubblesort.cpp
    Made the bubble sort optimized to decrease its best case complexity

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,15 +4,22 @@ using namespace std;
 void BubbleSort(int a[], int n)
 {
     int i, j, temp;
+    bool f = false;
     for(i=0;i<n;i++)
     {
         for(j=0;j<n-1;j++)
         {
+            f = false;
             if(a[j]>a[j+1])
             {
+                f = true;
                 temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp;
+            }
+            if(!f)
+            {
+                break;
             }
         }
     }
